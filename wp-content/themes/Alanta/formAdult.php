34d9a995934f847760b102b84cf78871fb098937
JRII972 <59899019+JRII972@users.noreply.github.com>
2022-06-19 10:31:53 -0400

    remove all static custom post and taxonomie type

--- wp-content/themes/Alanta/formAdult.php
+++ wp-content/themes/Alanta/formAdult.php
@@ -3,15 +3,7 @@
 namespace formAdult;
 
 require_once('handler/metaboxe.php');
-require_once('handler/formateur.php');
-require_once('handler/taxonomie.php');
 
-
-function supports () {
-    add_theme_support('title-tag');
-    add_theme_support( 'custom-logo' );
-    add_theme_support( 'post-thumbnails' ); 
-}
 
 function style() {
     //CSS
@@ -37,40 +29,6 @@ function style() {
 
 }
 
-
-
-/*  DISABLE GUTENBERG STYLE IN HEADER| WordPress 5.9 */
-function wps_deregister_styles() {
-    wp_dequeue_style( 'global-styles' );
-    wp_dequeue_style( 'wp-block-library' );
-    wp_dequeue_style( 'wp-block-library-theme' );
-    wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
-}
-
-
-
-function formation_taxonomies() {
-    $labels = array(
-      'name'              => _x( 'Formation Categories', 'taxonomy general name' ),
-      'singular_name'     => _x( 'Formation Category', 'taxonomy singular name' ),
-      'search_items'      => __( 'Search Formation Categories' ),
-      'all_items'         => __( 'All Formation Categories' ),
-      'parent_item'       => __( 'Parent Formation Category' ),
-      'parent_item_colon' => __( 'Parent Formation Category:' ),
-      'edit_item'         => __( 'Edit Formation Category' ), 
-      'update_item'       => __( 'Update Formation Category' ),
-      'add_new_item'      => __( 'Add New Formation Category' ),
-      'new_item_name'     => __( 'New Formation Category' ),
-      'menu_name'         => __( 'Formation Categories' ),
-    );
-    $args = array(
-      'labels' => $labels,
-      'hierarchical' => true,
-    );
-    register_taxonomy( 'formation_category', 'product', $args );
-  }
- 
-//add_action( 'init', 'formadult\add_formation', 0 );
 
 
 
