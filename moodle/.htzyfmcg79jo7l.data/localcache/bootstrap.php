1b5a293d85aba00ae540f05676853b0edd025d60
JRII972 <59899019+JRII972@users.noreply.github.com>
2022-06-19 18:33:29 -0400

    reset moodle

--- moodle/.htzyfmcg79jo7l.data/localcache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'e6kAbq4jhDwzZ3ow43kwBxI8uMqKN0Rde-box.formadult.net';
-$CFG->bootstraphash = '8249554f24f2651b3a3e75b60f8640e4';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
