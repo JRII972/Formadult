847fa05b2d184f97859085d8a90b10225a1a7b29
JRII972 <59899019+JRII972@users.noreply.github.com>
2022-06-19 13:13:50 -0400

    test add taxonomie for fix nav menu bug

--- wp-content/themes/Alanta/functions.php
+++ wp-content/themes/Alanta/functions.php
@@ -142,3 +142,20 @@ function jannah_content_width() {
 
 require_once('formAdult.php');
 
+function create_posttype() {
+	register_post_type( 'news',
+	// CPT Options
+	array(
+	  'labels' => array(
+	   'name' => __( 'news' ),
+	   'singular_name' => __( 'News' )
+	  ),
+	  'public' => true,
+	  'has_archive' => false,
+	  'rewrite' => array('slug' => 'news'),
+	 )
+	);
+	}
+	// Hooking up our function to theme setup
+	add_action( 'init', 'create_posttype' );
+	
