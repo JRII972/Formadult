4e50783ad63e01910c6c524eecf6aae26bca34d6
JRII972 <59899019+JRII972@users.noreply.github.com>
2022-06-26 22:35:16 -0400

    ...

--- wp-content/themes/Alanta/templates/template-full-size.php
+++ wp-content/themes/Alanta/templates/template-full-size.php
@@ -7,38 +7,10 @@
  * @subpackage Twenty_Twenty
  * @since Twenty Twenty 1.0
  */
-defined( 'ABSPATH' ) || exit; // Exit if accessed directly
-
-/**
- * The template for displaying the header
- *
- */
 
 defined( 'ABSPATH' ) || exit; // Exit if accessed directly
 
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	<script type="text/javascript" src="<?= get_template_directory_uri()?>/assets/js/formation.js?ver=6.0" id="formation-js"></script>
-	
-	<?php wp_head(); ?>
-</head>
-
-<body id="tie-body" <?php body_class(); ?>>
-
-<?php wp_body_open(); ?>
-
-<div class="background-overlay">
-
-	<div id="tie-container" class="site tie-container">
-
-		<?php do_action( 'TieLabs/before_wrapper' ); ?>
-
-		<div id="tie-wrapper">
-
-	
+get_header(); ?>
 
 <?php
 
@@ -46,7 +18,7 @@ if ( have_posts() ) :
 
 	while ( have_posts() ): the_post();
 
-		TIELABS_HELPER::get_template_part( 'templates/single-post/content-full-size' );
+		TIELABS_HELPER::get_template_part( 'templates/single-post/content' );
 
 	endwhile;
 
