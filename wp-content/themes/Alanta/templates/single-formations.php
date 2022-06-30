e2b4325e9b78572be65b594673dad5cd154d7288
JRII972 <59899019+JRII972@users.noreply.github.com>
2022-06-29 23:56:49 -0400

    ...

--- wp-content/themes/Alanta/templates/single-formations.php
+++ wp-content/themes/Alanta/templates/single-formations.php
@@ -497,13 +497,14 @@ $formationNbrAvis = get_comment_count(get_the_ID())['all'];
 															
 														</div>
 														<div class="sessions__col sessions_text"><span class="u-txt-session-available">12 participants maximum</span></div>
-														<div class="sessions__col" style="width: fit-content;">
+														<div class="sessions__col">
 															<div class="sessions__cta u-txt-center"><span 
-																	class="cta-default .cta-default--main" style="background: 2fadbb;">
-																	<a href="https://projetforma.com/formulaire-dinscription/" style="color: white;">S'inscrire</a></span>
+																	class="cta-default cta-default--main" style="color: white;">
+																	<a href="">S'inscrire</a></span>
 															</div>
 														</div>
 													</div>
+													
 													
 												</li>
 												
