0eb4015cf9f26ce98e572e146cd7f3556840d6d6
JRII972 <59899019+JRII972@users.noreply.github.com>
2023-05-05 12:04:54 -0400

    fix

--- wp-content/themes/Alanta/templates/single-formations.php
+++ wp-content/themes/Alanta/templates/single-formations.php
@@ -519,7 +519,7 @@ $formationNbrAvis = get_comment_count(get_the_ID())['all'];
 					<figure>
 						<?php 
 						foreach (rwmb_meta( 'formation_image_sup_prix', ['size' => 'medium'] ) as $value) {
-							echo '<img src="'.$value.'">'
+							echo '<img src="'.$value.'">';
 						}
 						?>
 					</figure>
